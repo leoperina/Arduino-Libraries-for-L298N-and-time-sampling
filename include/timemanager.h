349a07b215b9c83f63f61a6a39aa90634123cdad
leoperina <leonardoperina2003@gmail.com>
2026-03-23 23:28:24 +0100

    Update timemanager.h with english comments

--- include/timemanager.h
+++ include/timemanager.h
@@ -1,24 +1,23 @@
 #ifndef TIMEMANAGER_H
 #define TIMEMANAGER_H
 
-#define MAXF 100 * 1000.0f             // basically 100 kHz, ovvero Tsample = 10 microseconds. Realistitcamente questa frequeza è già troppo alta per essere seguita, e al netto sarebbe come chiedere BYPASS
+#define MAXF 100 * 1000.0f             // basically 100 kHz or Tsample = 10 microseconds. Realistically much more than useful but if you REALLY have a faster board modify this
 
 extern float         sampl_freq;    // Hz
 extern unsigned long samplT;        // microseconds
 extern unsigned long lastT;         // microseconds
-extern uint8_t       flag;          // è il modo in cui ci si interfaccia fra l'user e la libreria. Per capire cosa è cosa vedi flags
+extern uint8_t       flag;          // it is the way you inteface with the library, the value of "flag" is a value among SamplerState values
 
 enum SamplerState : uint8_t {
-  NOMINAL      , // tutto a posto, la frequenza in input è entro i limiti
-  FREQTOOHIGH  , // la frequenza richiesta è oltre la massima consentita, verrà usata MAXF
-  NEGATIVEFREQ , // la frequenza in input è negativa, provo ad invertirla e usarla, se è troppo alta uso MAXF
-  BYPASS         // la frequenza in input è zero. Lo prendo come un bypass a sample(), praticamente sto chiedendo la massima frequenza di campionamento possibile (rischiando però non sia costante per qual si voglia ragione)
-};
+  NOMINAL      , // all good, frequency is within bounds
+  FREQTOOHIGH  , // frequency is too high, MAXF will be used instead
+  NEGATIVEFREQ , // frequency is negative. If lower (in module) then MAXF I just remove the sign, otherwise I use MAXF
+  BYPASS         // basically I'm using the max possible frequency allowed by the board. It's exactly the same as not using the library and just letting the board live its course.
 
-void            beginSampler(float freq);
-bool            sample();
-int             returnFlag();
-float           returnSamplingFrequency();
-unsigned long   time_now();
+void            beginSampler(float freq);    // used in the setup() functions
+bool            sample();                    // used in the loop()
+int             returnFlag();                // used for debug or setup checks
+float           returnSamplingFrequency();   
+unsigned long   time_now();                  // internal function, ideally never used outside, it asks the board for the time at the moment
 
 #endif
